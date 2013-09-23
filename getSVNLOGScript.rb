7f69270d75d7d87f0b67b5b567dea5e3b2817a83
huangwanglin <huangwanglin@gmail.com>
2013-09-23 23:33:49 +0800

    SVN命令行执行调试通过

--- getSVNLOGScript.rb
+++ getSVNLOGScript.rb
@@ -1,37 +1,127 @@
-#encoding:utf-8
+
+#coding:utf-8
+####encoding:utf-8
 #
 #测试读取log的路径
 #http://bluefish-win.googlecode.com/svn/trunk/
 #http://yet-another-music-application.googlecode.com/svn/trunk
 
-#export RUBYOPT='-KU'
-#require 'iconv'
 
 require 'win32ole'
 #require 'Dir'
 require 'pathname'
 
+#这里直接通过块注释，先将有问题的代码注释掉
+
 class SvnOper
+    attr_reader:cmdStr, :httpPath, :userName, :password
 
-end.
-
-
-class ExcelOp
-    attr_reader:exApp, :exdbook, :exsheet
-
-    def initilize()
-        @exApp = WIN32OLE.new('Excel.Application')
-        #@exdbook = 
+    def initialize(http, userName, passWord)
+        @httpPath = http
+        @userName = userName
+        @password = passWord
     end
 
-    def createExcelFile(fileName)
-          if File.Exist(fileName) == false then
-              @exdbook = @exApp.Workbooks.add()
+    def svnLogCmd()
+        if (@userName == "") or (@password == "")
+            @cmdStr = "svn log " + @httpPath
+        else
+            @cmdStr = "svn log " + @httpPath + " --username " + @userName + " --password " + @password
+        end
+        
+    end
 
-          end
+    def excCmd()
+        svnLogCmd()
+        outPut = `#{cmdStr}`
+        puts "cmd excute......"
+        puts outPut
     end
 end
 
+=begin
+  
+class ExcelOp
+    attr_reader:exApp, :exdbook, :exsheet, :exsheets
+
+    def initialize()
+        @exApp = WIN32OLE.new('Excel.Application')
+        @exApp.visible = true
+        @exApp.displayalerts = false
+        #@exdbook = 
+    end
+
+    def openExcelFile(fileName)
+          #判断文件是否存在，如果不存在侧创建并保存
+          if File.Exist(fileName) == false
+              @exdbook = @exApp.Workbooks.add()
+              @exsheets = @exdbook.Worksheets
+              nCnt = 1
+              while nCnt <= @exsheets.Count()
+                  @exsheets(nCnt).Name = "LogOut" + nCnt
+              end
+              @exdbook.SaveAs(File.dirname(__FILE__) + fileName)
+          else
+              @exdbook = @exApp.Workbooks.Open(fileName)
+          end
+    end
+
+    def excelQuit()
+          @exdbook.close()
+          @exApp.Quit()
+    end
+end
+
+=end
+
+svnObj = SvnOper.new("http://yet-another-music-application.googlecode.com/svn/trunk", "", "")
+svnObj.excCmd()
+
+
+
+=begin
+class TestCode
+
+    def initialize()
+    end
+
+    def doPut()
+        puts "Hello world"
+        puts File.dirname(__FILE__)
+        #puts Dir.pwd
+        puts Pathname.new(File.dirname(__FILE__)).realpath
+
+        excel = WIN32OLE.new('Excel.Application')
+        excel.visible = true
+        excel.displayalerts = false
+        workbook = excel.Workbooks.Add()
+        worksheet = workbook.Worksheets(1)
+        worksheet.Range("A1:D1").value = ["North","South","East","West"];
+        worksheet.Range("A2:B2").value = [5.2, 10]
+        worksheet.Range("C2").value = 8
+        worksheet.Range("D2").value = 20
+        worksheet.Range("F3").value = "中国"
+
+        range = worksheet.Range("A1:D2")
+        range.select
+        chart = workbook.Charts.Add
+
+        workbook.SaveAs("E:\\Programming\\Script\\Ruby\\myfiletest.xls")
+        workbook.saved = true
+
+        workbook.close()
+
+        #excel.ActiveWorkbook.Close(0)
+        excel.Quit()
+    end
+end
+
+justPut = TestCode.new
+justPut.doPut()
+
+=end
+
+=begin
 puts File.dirname(__FILE__)
 #puts Dir.pwd
 puts Pathname.new(File.dirname(__FILE__)).realpath
@@ -58,4 +148,6 @@ workbook.close()
 
 #excel.ActiveWorkbook.Close(0)
 excel.Quit()
+
+=end
 
